fcc7b40ba768eda6b62523420bc93df0ee7cb90b
yuting <tingle2008@gmail.com>
2017-07-23 11:54:43 -0700

    prerm remove added

--- root/usr/share/multipkg/scripts/supervisepreun.sh
+++ root/usr/share/multipkg/scripts/supervisepreun.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-if [ "$1" = "0" ]; then
+if [ "$1" = "remove" ]; then
 	rm /service/%service%
 	svc -dx /etc/service/%service% /etc/service/%service%/log
 fi
 
+if [ "$1" = "0" ]; then
+	rm /service/%service%
+	svc -dx /etc/service/%service% /etc/service/%service%/log
+fi
